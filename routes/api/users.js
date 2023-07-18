2fcec69dad766e6b59967f6b3adca037eb952959
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-07-18 14:33:43 +0300

    users route draft done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,66 +1,96 @@
 const express = require("express");
 const router = express.Router();
+const usersServiceModel = require("../../model/usersService/usersService");
+const usersValidationService = require("../../validation/usersValidationService");
+const normalizeUser = require("../../model/usersService/helpers/normalizationUserService");
+const loggedInMiddleware = require("../../middlewares/checkLoggedInMiddleware");
+const permissionsMiddleware = require("../../middlewares/permissionsMiddleware");
+const CustomError = require("../../utils/CustomError");
+const hashService = require("../../utils/hash/hashService");
+const { generateToken } = require("../../utils/token/tokenService");
+const finalCheck = require("../../utils/finalResponseChecker");
+const initialValidationService = require("../../utils/initialValidationCheckers");
 
 //Get all users, authorization : Admin, Return : array of users
-router.get("/", (req, res) => {
-    console.log("in users get all users");
-    res.json({ msg: "in users get all users" });
+router.get("/", loggedInMiddleware, permissionsMiddleware(false, true, false, false), async (req, res) => {
+    const allUsers = await usersServiceModel.getAllUsers();
+    res.status(200).json(allUsers);
 });
 
 //Get specific user, authorization : Admin or registered user, Return : User
-router.get("/:id", (req, res) => {
-    const idValue = 111;
-    console.log(req.params.id);
-    console.log("are ids equal ");
-    console.log(idValue == req.params.id);
-    console.log("in users get all users");
-    res.json({ msg: "in users get all users" });
+router.get("/:id", loggedInMiddleware, permissionsMiddleware(false,true,false,true), async (req, res) => {
+    let idTest = await initialValidationService.initialJoiValidation(usersValidationService.userIdValidation, req.params.id);
+    if(!idTest[0]) return next(new CustomError(400, idTest[1]));
+    let wantedUser = await usersServiceModel.getUserById(req.params.id);
+    finalCheck(res, wantedUser, 404, "User Not Found");
 });
 
 //Register User, authorization : all, return : registered user, needs unique email
-router.post("/", (req, res) => {
-    console.log("in users post register ");
-    res.json({ msg: "in users post register " });
+router.post("/", async (req, res) => {
+    let registerBodyTest = await initialValidationService.initialJoiValidation(usersValidationService.registerUserValidation, req.body);
+    if(!registerBodyTest[0]) return next(new CustomError(400,registerBodyTest[1]));
+    let checkIfEmailIsTaken = await usersServiceModel.getUserByEmail(req.body.email);
+    if(checkIfEmailIsTaken) return next(new CustomError(400, "Email Already Taken"));
+    let normalizedUser = await normalizeUser(req.body);
+    normalizedUser.password = await hashService.generateHash(normalizedUser.password);
+    let createdUser = await usersServiceModel.registerUser(normalizedUser);
+    finalCheck(res, createdUser, 500, "Registration went wrong");
 });
 
 //Login User, authorization : all, return : Encrypted token
-router.post("/login", (req,res) =>{
-    console.log("in users post login");
-    res.json({ msg: "in users post login" });
+router.post("/login", async (req,res) =>{
+    let loginBodyTest = await initialValidationService.initialJoiValidation(usersValidationService.loginUserValidation, req.body);
+    if(!loginBodyTest[0]) return next(new CustomError(400,loginBodyTest[1]));
+    const userData = await usersServiceModel.getUserByEmail(req.body.email);
+    if (!userData) return next(new CustomError(400,"Invalid email"));
+    const isPasswordMatch = await hashService.cmpHash(
+        req.body.password,
+        userData.password
+    );
+    if (!isPasswordMatch)
+    {
+        return next(new CustomError(403,"Invalid password"));
+    }
+    const token = await generateToken({
+        _id: userData._id,
+        isDoctor: userData.isDoctor,
+        isAdmin: userData.isAdmin,
+    });
+    res.status(200).json({ token });
 })
 
 //Edit user, authorization : The registered user, Return : The edited user
-router.put("/:id", (req, res) => {
-    const idValue = 111;
-    console.log(req.params.id);
-    console.log("are ids equal ");
-    console.log(idValue == req.params.id);
-    console.log("in users put, Edit User");
-    res.send("in users put, Edit User");
-    //res.json({msg: "in cards put"});
+router.put("/:id", loggedInMiddleware, permissionsMiddleware(false, false, false, true), async (req, res) => {
+    let idTest = await initialValidationService.initialJoiValidation(usersValidationService.userIdValidation, req.params.id);
+    if(!idTest[0]) return next(new CustomError(400, idTest[1]));
+    let editBodyTest = await initialValidationService.initialJoiValidation(usersValidationService.profileUserValidation, req.body);
+    if(!editBodyTest[0]) return next(new CustomError(400,editBodyTest[1]));
+    if(req.body.email){
+        let checkIfEmailIsTaken = await usersServiceModel.getUserByEmail(req.body.email);
+        if(checkIfEmailIsTaken) return next(new CustomError(400, "Email Already Taken"));
+    }
+    let normalizedEditedUser = normalizeUser(req.body);
+    if(normalizedEditedUser.password){
+        normalizedEditedUser.password = await hashService.generateHash(normalizedEditedUser.password);
+    }
+    let editResult = await usersServiceModel.updateUser(req.params.id, normalizedEditedUser);
+    finalCheck(res, editResult, 400, "User to edit not found");
 })
 
-//Change is business status, authorization : The registered user, Return : The User
-router.patch("/:id", (req, res) => {
-    const idValue = 111;
-    console.log(req.params.id);
-    console.log("are ids equal ");
-    console.log(idValue == req.params.id);
-    console.log("in users patch");
-    res.send("in users patch");
-    //res.json({msg: "in cards put"});
+//Change is Doctor status, authorization : The registered user, Return : The User
+router.patch("/:id", loggedInMiddleware, permissionsMiddleware(false,true,false,true), async (req, res) => {
+    let idTest = await initialValidationService.initialJoiValidation(usersValidationService.userIdValidation, req.params.id);
+    if(!idTest[0]) return next(new CustomError(400, idTest[1]));
+    let businessStatusUpdateResult = await usersServiceModel.changeDoctorStatusById(req.params.id);
+    finalCheck(res, businessStatusUpdateResult, 400, "User to update not found");
 })
 
 //Delete User, Authorization : The registered User or Admin, return : The Deleted User
-router.delete("/:id", (req, res) => {
-    const idValue = 111;
-    console.log(req.params.id);
-    console.log("are ids equal ");
-    console.log(idValue == req.params.id);
-    console.log("in users delete");
-    res.send("in users delete");
-    //res.json({msg: "in cards put"});
+router.delete("/:id", loggedInMiddleware, permissionsMiddleware(false, true, false, true), async (req, res) => {
+    let idTest = await initialValidationService.initialJoiValidation(usersValidationService.userIdValidation, req.params.id);
+    if(!idTest[0]) return next(new CustomError(400, idTest[1]));
+    let deletedUser = await usersServiceModel.deleteUserById(req.params.id);
+    finalCheck(res, deletedUser, 400, "User to delete not found");
 })
-
 
 module.exports = router;
