5e537cc6c99490f116e277f3409d86bcd4f42058
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-07-26 10:47:57 +0300

    comments deleted

--- model/mongoDB/prescriptions/prescriptionsServiceMdb.js
+++ model/mongoDB/prescriptions/prescriptionsServiceMdb.js
@@ -22,8 +22,6 @@ const getPrescriptionsOfSpecificDoctor = (id) => {
 }
 
 const getPrescriptionWithSubItem = (subItemId) => {
-    //return Prescription.find({ "_id": prescriptionId, "medicineList._id": subItemId });
-    console.log("here");
     return Prescription.findOne({ medicineList: { $elemMatch: { _id: subItemId } } }, { "medicineList.$": 1 });
 };
 
@@ -35,22 +33,6 @@ const updatePrescriptionSubItem = async (prescriptionId, subItemId) => {
     let prescriptionWithSubItem = await Prescription.findOne({ medicineList: { $elemMatch: { _id: subItemId } } }, { "medicineList.$": 1 });
     return Prescription.findOneAndUpdate({ "_id": prescriptionId, "medicineList._id": subItemId },{ "$set": { "medicineList.$.isActive": !prescriptionWithSubItem.medicineList[0].isActive } } , {new:true} );
 }
-
-//template
-//// let prescriptionSubItemStatus = await Prescription.findOne({ "_id": prescriptionId, "medicineList._id": subItemId });
-// console.log(prescriptionSubItemStatus);
-
-//works but only set value
-//return Prescription.findOneAndUpdate({ "_id": prescriptionId, "medicineList._id": subItemId },{ "$set": { "medicineList.$.isActive": false } } , {new:true} );
-
-//doesnt work
-//return Prescription.findOneAndUpdate({ "_id": prescriptionId, "medicineList._id": subItemId },{ "$set": { "medicineList.$.isActive": { "$eq": [false, "$medicineList.$.isActive"] } } } , {new:true} );
-
-
-//doesnt work
-// return Prescription.findOneAndUpdate({ "_id": prescriptionId, "medicineList._id": subItemId },[{ $set: { "medicineList.$.isActive": { $not: "medicineList.$.isActive" } } }] , {new:true} )
-
-// return Prescription.findOneAndUpdate({ "_id": prescriptionId, "medicineList._id": subItemId },{ "$set": { "medicineList.$.isActive": { $not : "medicineList.$.isActive" } } } , {new:true} )
 
 const changeActiveStatusById = (id) => {
     return Prescription.findByIdAndUpdate(id, [{ $set: { isActive: { $not: "$isActive" } } }], {new:true});
