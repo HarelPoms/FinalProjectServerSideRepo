5f917366cf197d7206383e6f9c3e29673296150a
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-08-24 16:39:39 +0300

    Changed prescription validation to allow editing of prescriptions with existing medicine lists

--- validation/joi/prescriptionValidation.js
+++ validation/joi/prescriptionValidation.js
@@ -1,10 +1,18 @@
 const Joi = require("joi").extend(require('@joi/date')); 
 
-let prescriptionSubItemJoiSchema = Joi.object().keys({
+let prescriptionSubItemTemplate = {
     medicineId: Joi.number().min(1000000).max(9999999).allow(""),
     medicineName: Joi.string().required(),
     medicineUnits: Joi.number().integer().min(1).max(5).required(),
     isActive: Joi.boolean().required()
+}
+
+let prescriptionSubItemCreationJoiSchema = Joi.object().keys({
+    ...prescriptionSubItemTemplate
+})
+
+let prescriptionSubItemEditJoiSchema = Joi.object().keys({
+    ...prescriptionSubItemTemplate, _id: Joi.string().hex().length(24)
 })
 
 const createPrescriptionSchema = Joi.object({
@@ -14,7 +22,7 @@ const createPrescriptionSchema = Joi.object({
             /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/)).allow(""),
         alt: Joi.string().min(2).max(256).required().allow(""),
     }),
-    medicineList: Joi.array().items(prescriptionSubItemJoiSchema),
+    medicineList: Joi.array().items(prescriptionSubItemCreationJoiSchema),
     patientId: Joi.string().hex().length(24).required(),
     doctorId: Joi.string().hex().length(24),
     expiryDate: Joi.date().format('YYYY-MM-DD HH:mm'),
@@ -28,7 +36,7 @@ const editPrescriptionSchema = Joi.object({
             /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/)).allow(""),
         alt: Joi.string().min(2).max(256).required(),
     }),
-    medicineList: Joi.array().items(prescriptionSubItemJoiSchema),
+    medicineList: Joi.array().items(prescriptionSubItemEditJoiSchema),
     patientId: Joi.string().hex().length(24),
     doctorId: Joi.string().hex().length(24).allow(""),
     expiryDate: Joi.date().format('YYYY-MM-DD HH:mm'),
